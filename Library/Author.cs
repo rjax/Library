845ef4196fb0bb902216ab01b53924c8f3141ad2
Reuven <reuven.jackson@1beat.com>
2021-11-11 12:05:38 +0200

    Step 4: Get books by author

--- Library/Author.cs
+++ Library/Author.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HotChocolate;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,8 +9,13 @@ namespace Library
     public class Author
     {
         public int Id { get; set; }
-        public string Bio { get; set; }
-        public string Name { get; set; }
+        public string? Bio { get; set; }
+        public string Name { get; set; } = default!;
         public DateTime BirthDate { get; set; }
+
+        public List<Book> Books([Parent] Author author)
+        {
+            return BookList.Books.Where(b => b.AuthorId == author.Id).ToList();
+        }
     }
 }
